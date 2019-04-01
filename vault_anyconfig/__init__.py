7884944b1e94a009025a45e93943b92dd1f762cc
tt-ci <noreply@tomtom.com>
2019-04-01 11:13:42 +0000

    Bump version: 0.0.3 -> 0.0.4-dev ***NO_CI***

--- vault_anyconfig/__init__.py
+++ vault_anyconfig/__init__.py
@@ -2,7 +2,7 @@
 
 from datetime import datetime
 
-__version__ = "0.0.3"
+__version__ = "0.0.4-dev"
 __author__ = "Eugene Davis"
 __author_mail__ = "eugene.davis@tomtom.com"
 __copyright__ = "%s, TomTom International B.V." % datetime.now().year
