09fe965999f5832156aae8b3d82a87595e483226
Martin Migasiewicz <616250+martinm82@users.noreply.github.com>
2019-05-20 14:26:09 +0200

    Bump version: 0.2.0-dev → 0.2.1-dev (#20)

--- vault_anyconfig/__init__.py
+++ vault_anyconfig/__init__.py
@@ -2,7 +2,7 @@
 
 """Top-level package for vault-anyconfig."""
 
-__version__ = "0.2.0-dev"
+__version__ = "0.2.1-dev"
 __project__ = "vault-anyconfig"
 __email__ = "eugene.davis@tomtom.com"
 __author__ = "Eugene Davis"
