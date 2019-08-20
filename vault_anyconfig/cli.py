5ac0b9440bf313784da158d1e741e4d10a9f08ec
Eugene Davis <eugene.davis@tomtom.com>
2019-08-20 10:54:12 +0200

    Typographical cleanup

--- vault_anyconfig/cli.py
+++ vault_anyconfig/cli.py
@@ -18,7 +18,7 @@ def parse_args(args):
     parser.add_argument("out_file", type=argparse.FileType("w"), help="File to write out after populating")
     parser.add_argument("--file_type", type=str, required=True, help="File type to read and write")
     parser.add_argument(
-        "--vault_config", type=str, required=True, help="Vault configuration file  as string or file path."
+        "--vault_config", type=str, required=True, help="Vault configuration file as string or file path."
     )
     parser.add_argument("--vault_creds", type=str, required=True, help="Vault credentials as string or file path")
     parser.add_argument(
