5292319c483a56108d6a1d6888520c964ae185b2
tt-ci <44620811+tt-ci@users.noreply.github.com>
2019-08-20 14:07:30 +0000

    Bump version: 0.3.1 -> 0.3.2-dev ***NO_CI***

--- vault_anyconfig/__init__.py
+++ vault_anyconfig/__init__.py
@@ -2,7 +2,7 @@
 
 """Top-level package for vault-anyconfig."""
 
-__version__ = "0.3.1"
+__version__ = "0.3.2-dev"
 __project__ = "vault-anyconfig"
 __email__ = "eugene.davis@tomtom.com"
 __author__ = "Eugene Davis"
